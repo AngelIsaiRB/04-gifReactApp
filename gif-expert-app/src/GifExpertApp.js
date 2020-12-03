5f1352cfc6c331defc1e8bfb272e58752dcbc48f
Anegelisai <63125934+Anegelisai@users.noreply.github.com>
2020-12-02 20:34:18 -0600

    form inputs

--- gif-expert-app/src/GifExpertApp.js
+++ gif-expert-app/src/GifExpertApp.js
@@ -1,19 +1,23 @@
 // rafc
 import React, { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
 
 // const categories=["one pucnh", "samurai", "drangon ball"];
 
 const GifExpertApp =()=>{
+    
+    
     const [categories, setCategories] = useState(["one pucnh", "samurai", "drangon ball"]);
 
-    const handleAdd=(value)=>{
-        setCategories([...categories,value]);
-        // setCategories(cats => [...cats,value]);
-    }
+    // const handleAdd=(value)=>{
+    //     setCategories([...categories,value]);
+    //     // setCategories(cats => [...cats,value]);
+    // }
     return  <>
         <h2>GifExpertApp</h2>
+        <AddCategory />
         <hr/>
-        <button onClick={()=>handleAdd("valor")}>agregar</button>
+       
         <ol>
             {
                 categories.map((category, i)=>{
--- gif-expert-app/src/components/AddCategory.js
+++ gif-expert-app/src/components/AddCategory.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const AddCategory = () => {
+    
+    const [inputValue, setInputValue] = useState("hola mundo")
+    const handleInputchange = (e)=>{
+        setInputValue(e.target.value)
+    }
+
+    const handleSubmit =(e)=>{
+        e.preventDefault();
+        console.log("submi echoo")
+    }
+
+
     return (
-        <>
-            <h2>Add Category</h2>   
-        </>
+      
+           <form onSubmit={handleSubmit}>
+           <input
+                type="text"
+                value={inputValue}
+                onChange={handleInputchange} 
+           ></input>
+           </form>
+
+       
     )
 }
