1eb7fe63163f5807d5cb5a7ae80390ebb94aba18
Anegelisai <63125934+Anegelisai@users.noreply.github.com>
2020-12-04 20:20:50 -0600

    simulate actions

--- gif-expert-app/src/test/components/AddCategory.test.js
+++ gif-expert-app/src/test/components/AddCategory.test.js
@@ -5,14 +5,32 @@ import React, { PureComponent } from 'react'
 
 describe('pruebas en AddCategory', () => {
     // const wrapper = 
-    const setCategories=()=>{
+    const setCategories=jest.fn();
+    let wrapper = shallow(<AddCategory setCategories={setCategories} /> );
 
-    };
-
+    beforeEach(()=>{
+        jest.clearAllMocks();
+         wrapper = shallow(<AddCategory setCategories={setCategories} /> );
+    });
 
     test('should de de mostrarse correctamente', () => {
-        const wrapper = shallow(<AddCategory setCategories={setCategories} /> );
         expect(wrapper).toMatchSnapshot();
     });
+    
+    test('should simular cuando la caja de texto cambia', () => {
+        const input = wrapper.find("input");
+        const value= "Holamundo";
+        input.simulate("change",{target:{value:value}});
+        expect(wrapper.find("p").text().trim()).toBe(value);
+    });
+
+    test('should No debe de posterar la informacin con submit', () => {
+        wrapper.find("form").simulate("submit",{
+            preventDefault(){}
+        });
+        expect(setCategories).not.toHaveBeenCalled();
+        
+    })
+    
     
 });
