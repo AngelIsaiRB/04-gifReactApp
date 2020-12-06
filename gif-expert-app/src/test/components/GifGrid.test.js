8d8d3a7fce952771ff152ed6ddbec1d71e15c1a8
Anegelisai <63125934+Anegelisai@users.noreply.github.com>
2020-12-05 19:36:32 -0600

    simular functions useFetchGifs

---
+++ gif-expert-app/src/test/components/GifGrid.test.js
@@ -0,0 +1,38 @@
+const { GifGrid } = require("../../components/GifGrid");
+import React from 'react'
+import { shallow } from 'enzyme';
+import { useFetchGifs } from '../../hooks/useFetchGifs';
+jest.mock('../../hooks/useFetchGifs');
+
+describe("pruebas dentro de gifGrid",  ()=>{
+    const category="hola world";
+
+    test('should de hacr match con el snapshot', () => {
+
+        useFetchGifs.mockReturnValue({
+            data    : [],
+            loading :true,
+        });
+
+        const wrapper = shallow(<GifGrid   category={category}/>);
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    test('should debe de mostrar items cuando carga imagenes useFetchsGisf', () => {
+        const gifs =[{
+            id:"ABC",
+            url:"Https://localhostfnsofbsofudnf",
+            title: "xx nada xx"
+        }]
+        useFetchGifs.mockReturnValue({
+            data    : [],
+            loading :true,
+        });
+        const wrapper = shallow(<GifGrid   category={category}/>);
+        
+    });
+    
+    
+
+});
+
